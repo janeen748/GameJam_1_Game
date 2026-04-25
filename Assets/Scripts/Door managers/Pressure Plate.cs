d7a61f3c2f2362cafe6195f910e6a55e219e1bc9
simba29116 <simesihle@icloud.com>
2026-04-25 13:58:48 +0200

    Button Functionality
    Button and animations done

---
+++ Assets/Scripts/Door managers/Pressure Plate.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject door;
+    public float pressDepth = 0.1f;
+    private Vector3 startPos;
+    private int objectsOnTop = 0;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectsOnTop++;
+                UpdateState();
+
+            Debug.Log("Player");
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectsOnTop--;
+            UpdateState();
+
+            Debug.Log("No touching");
+        }
+    }
+
+    void UpdateState()
+    {
+        if(objectsOnTop > 0)
+        {
+            transform.position = startPos - new Vector3(0, pressDepth, 0);
+
+            
+            door.GetComponent<Animator>().SetTrigger("DoorPressed");
+        }
+        else
+        {
+            transform.position = startPos;
+            
+            door.GetComponent<Animator>().SetTrigger("DoorClose");
+        }
+    }
+}
